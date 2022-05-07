653c860da8b59ffff18ed1e0892dd81e3efd581e
reficul31 <reficul31@gmail.com>
2022-05-07 10:29:06 -0400

    Breaking the _init__ file

--- kfold/__init__.py
+++ kfold/__init__.py
@@ -1,39 +1,6 @@
-import os
-import time
-import json
-
-from .jobscheduler import JobScheduler
-from .waiter import Waiter
-
-class KFold(object):
-    def __init__(self, kfolds, train_dataset, root_dir, criterion, batch_size, parallelize=True):
-        self.waiter = Waiter(train_dataset, criterion, batch_size, kfolds)
-        self.root_dir = root_dir
-        self.parallelize = parallelize
-
-    def __call__(self, models, schedulers, get_optimizer, trainer_params):
-        for net in models:
-            name = net.__name__
-            if not os.path.exists(os.path.join(self.root_dir, name)):
-                os.mkdir(os.path.join(self.root_dir, name))
-            
-            for scheduler_name in schedulers:
-                root_dir = os.path.join(self.root_dir, name, scheduler_name)
-                if not os.path.exists(root_dir):
-                    os.mkdir(root_dir)
-                
-                waiter_params = {
-                    "net": net,
-                    "get_optimizer": get_optimizer,
-                    "scheduler_name": scheduler_name,
-                    "root_dir": root_dir,
-                    "trainer_params": trainer_params
-                }
-                
-                job_scheduler = JobScheduler()
-                start = time.time()
-                job_scheduler(self.waiter, waiter_params, self.parallelize)
-                end = time.time()
-
-                with open(os.path.join(root_dir, 'time.json'), 'w') as time_file:
-                    time_file.write(json.dumps(dict({'start': start, 'end': end})))
+from .kfold import *
+from .jobscheduler import *
+from .tester import *
+from .trainer import *
+from .utils import *
+from .waiter import *
