04af81415693dc8e4fb143851945876bad13188d
reficul31 <reficul31@gmail.com>
2022-05-07 12:36:02 -0400

    Changing number of input channels in alexnet

--- models/alexnet.py
+++ models/alexnet.py
@@ -4,7 +4,7 @@ class AlexNet(Module):
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.features = Sequential(
-            Conv2d(3, 64, kernel_size=11, stride=4, padding=5),
+            Conv2d(1, 64, kernel_size=11, stride=4, padding=5),
             ReLU(inplace=True),
             MaxPool2d(kernel_size=2, stride=2),
             Conv2d(64, 192, kernel_size=5, padding=2),
@@ -24,4 +24,4 @@ class AlexNet(Module):
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
-        return 
+        return x
